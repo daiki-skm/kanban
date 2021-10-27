5f81db94c477b6b0b0b0c83735599f09d60a5e3d
daiki328 <dk08sel11@i.softbank.jp>
2021-10-27 18:03:40 +0900

    add: new geo

---
+++ components/Geo.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="artwork">
+    <canvas class="artwork__canvas" ref="canvas"></canvas>
+  </section>
+</template>
+
+<script>
+import Vue from 'vue'
+import * as THREE from "three";
+
+export default Vue.extend({
+  name: 'Geo',
+  mounted () {
+    const width = 960;
+    const height = 540;
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas: this.$refs.canvas
+    });
+    renderer.setSize(width, height);
+
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+    camera.position.set(0, 500, +1000);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    const container = new THREE.Object3D();
+    scene.add(container);
+
+    const material = new THREE.MeshStandardMaterial({
+        color: 0x87CEEB,
+        side: THREE.DoubleSide,
+    });
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
+    const ambientLight = new THREE.AmbientLight(0x999999);
+    scene.add(ambientLight);
+
+    const geometryList = [
+        new THREE.SphereGeometry(50), // 球体
+        new THREE.BoxGeometry(100, 100, 100), // 直方体
+        new THREE.PlaneGeometry(100, 100), // 平面
+        new THREE.TetrahedronGeometry(100, 0), // 三角錐
+        new THREE.ConeGeometry(100, 100, 32), // 円錐
+        new THREE.CylinderGeometry(50, 50, 100, 32), // 円柱
+        new THREE.TorusGeometry(50, 30, 16, 100), // ドーナツ形状
+    ];
+
+    geometryList.map((geometry, index) => {
+        const mesh = new THREE.Mesh(geometry, material);
+
+        // 3D表示インスタンスのsceneプロパティーが3D表示空間となる
+        container.add(mesh);
+
+        // 円周上に配置
+        mesh.position.x = 400 * Math.sin((index / geometryList.length) * Math.PI * 2);
+        mesh.position.z = 400 * Math.cos((index / geometryList.length) * Math.PI * 2);
+    });
+
+    tick()
+
+    function tick() {
+        container.rotation.y += 0.01
+        renderer.render(scene, camera)
+
+        requestAnimationFrame(tick)
+    }
+  },
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <Nav />
+    <!-- <Nav /> -->
+    <Geo />
   </div>
 </template>
 
