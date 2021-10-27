d72a8507d27bcf0ad07119477fe60ddb57a0b0fd
daiki328 <dk08sel11@i.softbank.jp>
2021-10-27 17:33:08 +0900

    confirm: geometry

--- components/Nav.vue
+++ components/Nav.vue
@@ -7,42 +7,76 @@
 <script>
 import Vue from 'vue'
 import * as THREE from "three";
+import img from "@/static/earth.jpg"
 
 export default Vue.extend({
   name: 'Nav',
   mounted () {
-    // サイズを指定
     const width = 960;
     const height = 540;
 
-    // レンダラーを作成
     const renderer = new THREE.WebGLRenderer({
       canvas: this.$refs.canvas
     });
-    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(width, height);
 
-    // シーンを作成
+    // 3D空間、3Dオブジェクトや光源などの置き場
     const scene = new THREE.Scene();
 
-    // カメラを作成
-    const camera = new THREE.PerspectiveCamera(45, width / height);
+    // どの視点から空間を撮影するか（画角、アスペクト比、描画開始距離、描画終了距離）
+    const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
     camera.position.set(0, 0, +1000);
 
-    // 箱を作成
-    const geometry = new THREE.BoxGeometry(400, 400, 400);
-    const material = new THREE.MeshNormalMaterial();
-    const box = new THREE.Mesh(geometry, material);
-    scene.add(box);
+    // ジオメトリ：頂点情報や面情報を持っている
+    // マテリアル：色や質感の情報を持っている、着色や画像・陰影の割り当て、ライティングの反射などを適用できる
+
+    // 箱
+    // const geometry = new THREE.BoxGeometry(400, 400, 400);
+    // 球体
+    const geometry = new THREE.SphereGeometry(300, 30, 30);
+    // 平面
+    // const geometry = new THREE.PlaneGeometry( 400, 300, 300 );
+    // 三角錐
+    // const geometry = new THREE.ConeGeometry( 200, 400, 400 );
+    // 円柱
+    // const geometry = new THREE.CylinderGeometry( 200, 200, 400, 100 )
+    // ドーナツ状
+    // const geometry = new THREE.TorusGeometry( 200, 70, 200, 200 );
+
+    // ノーマル色
+    // const material = new THREE.MeshNormalMaterial();
+    // 単色塗り
+    const material = new THREE.MeshStandardMaterial({color: 0x87CEEB});
+    // 強制環境光？
+    // const material = new THREE.MeshBasicMaterial({color: 0x87CEEB});
+    // 平面の時、裏面も色を塗りたいため
+    // const material = new THREE.MeshBasicMaterial({color: 0x87CEEB, side: THREE.DoubleSide});
+
+    // 画像（GPUの制約から、2の累乗の高さ・幅である画像のみ利用可能）
+    // const loader = new THREE.TextureLoader();
+    // const texture = loader.load('static/demo.jpg');
+    // const material = new THREE.MeshStandardMaterial({map: texture});
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // 平行光源
+    const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+    // 環境光
+    // const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
+    // scene.add(ambientLight);
 
     tick()
 
-    // 毎フレーム時に実行されるループイベントです
     function tick() {
-      box.rotation.y += 0.01;
-      renderer.render(scene, camera); // レンダリング
+      // mesh.rotation.x += 0.01
+      mesh.rotation.y += 0.01
+      // mesh.rotation.z += 0.01
+      renderer.render(scene, camera)
 
-      requestAnimationFrame(tick);
+      requestAnimationFrame(tick)
     }
   },
 })
